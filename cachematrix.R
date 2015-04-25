84c66bfc86c45e249f305b6a688645643493194a
mike-glennon <mike.glennon@ymail.com>
2015-04-25 17:45:17 +0100

    working version?

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,14 @@
-## this function creates a special matrix that can cache its inverse, when called by cacheSolve
 ## this is programming assignment 2
+## these two functons form a pair, which create a special matrix which can cache its inverse.
+## and create the inverse of the function
+## this function creates a special matrix that can cache its inverse, when called by cacheSolve
+
 
 ## Mike Glennon 25-Apr-2015
 
 makeCacheMatrix <- function(x = matrix()) {
       x.inv <- NULL
-      # I don't think set is used
+      # I don't think set is used, but still included here as it is part of the list vector
       set <- function(y) {
             x <<- y
             x.inv <<- NULL
@@ -15,20 +18,22 @@ makeCacheMatrix <- function(x = matrix()) {
       getinv <- function() x.inv                 ## just returns m when get is called. e.g. x$getinv() returns x.inv
       list(set = set, get = get,
            setinv = setinv,
-           getinv = getinv)
+           getinv = getinv)                     # outputs the list of functions created as a list. these are called using
+                                                # x$get(), x$set(new value) etc.
 }
 
-## This calculates the inverse of the special matrix created above, unless a cache copy exists. in which case it takes that
+## This function calculates the inverse of the special matrix created above, 
+## unless a cache copy exists. in which case it takes that
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-      x.inv<-x$getinv()
-      if(!is.null(x.inv)) {
+      x.inv<-x$getinv()                         ## uses getinv to returned the cached version
+      if(!is.null(x.inv)) {                     ## if x.inv exists then use it, otherwise cal new one
             messsage("getting cached inverse")
-            return(x.inv)
+            return(x.inv)                       ## exit the function, returning the cached inverse
       }
-      data<-x$get()
-      x.inv<-solve(data)
-      x$setinv(x.inv)
-      x.inv
+      data<-x$get()                             ## get the new matrix if the cached copy doesn;'t exist
+      x.inv<-solve(data)                        ## use solve to calculate the inverse. Assume is invertable
+      x$setinv(x.inv)                           ## write the new version to the cache using setinv
+      x.inv                                     ## exit with the value of the inverted matrix
 }
